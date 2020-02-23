4f89b6e810c59b601e3b63adc4ea16cc0699e961
Duong Dang <dddangmedia@gmail.com>
2020-02-23 05:41:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,6 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  
-  puts 'display board'
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
